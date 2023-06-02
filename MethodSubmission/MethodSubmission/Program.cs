84bb1dab3092eed9bd0b01a95f85dbe3a62cb441
themmm19 <melhannach@gmail.com>
2023-06-02 16:11:44 -0700

    Method class submission

---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class.
+            SimpleMethod simpleMethod = new SimpleMethod();
+
+            //Call the method in the class, passing in two numbers. 
+            simpleMethod.SimpleMath(5, 60);
+            
+
+            //Call the method in the class, specifying the parameters by name.
+            simpleMethod.SimpleMath(numb1: 10, numb2: 20);
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/SimpleMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class SimpleMethod
+    {
+        //a void method that takes two integers as parameters.
+        public void SimpleMath(int numb1, int numb2)
+        {
+            //Have the method do a math operation on the first integer and display the second integer to the screen.
+            int result = numb1 + numb2;
+            Console.WriteLine(result);
+            
+        }
+
+         
+    }
+}
