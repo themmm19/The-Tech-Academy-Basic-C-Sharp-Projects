edcf180e2e33df5bec6d264a3e0cb48f79e06489
themmm19 <melhannach@gmail.com>
2023-06-02 18:34:18 -0700

    Operators Assignment Submission

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        //employee properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //overriding == and !=
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1.Id == emp2.Id);
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { Id = 1};
+            Employee employee2 = new Employee() { Id = 2};
+            Employee employee3 = new Employee() { Id = 1};
+
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1 == employee3);
+            Console.ReadLine();
+        }
+    }
+}
