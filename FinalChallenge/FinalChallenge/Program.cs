23ab7a5cb3769aecc08fc25a6c46950600c3a0e0
themmm19 <melhannach@gmail.com>
2023-06-14 15:51:37 -0700

    final challenge

---
+++ FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using FinalChallenge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "name of student" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
